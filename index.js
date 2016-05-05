1935d961136e1c4af0e41c09d1f5c20afe8bdf9a
Nathanael Burt <nburt@admins-MacBook-Pro.local>
2016-05-05 15:51:09 -0600

    display todos

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 
-var HelloWorld = Marionette.LayoutView.extend({
-    el: '#app-hook',
-    template: require('./templates/layout.html')
+var ToDo = Marionette.LayoutView.extend({
+    tagName: 'li',
+    template: require('./templates/todoitem.html')
 });
 
-var hello = new HelloWorld();
-hello.render();
+var TodoList = Marionette.CollectionView.extend({
+    el: '#app-hook',
+    tagName: 'ul',
+    childView: ToDo
+});
+
+var todo = new TodoList({
+    collection: new Backbone.Collection([
+        {assignee: 'Nate', text: 'Learn Marionette'},
+        {assignee: 'Nate', text: 'Learn Backbone'}
+    ])
+});
+
+todo.render();
