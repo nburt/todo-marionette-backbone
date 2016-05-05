c30f6da6cde8874fd1f83ab0c2d3e8659797955f
Nathanael Burt <nburt@admins-MacBook-Pro.local>
2016-05-05 15:38:43 -0600

    hello, world

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+var Marionette = require('backbone.marionette');
+
+var HelloWorld = Marionette.LayoutView.extend({
+    el: '#app-hook',
+    template: require('./templates/layout.html')
+});
+
+var hello = new HelloWorld();
+hello.render();
